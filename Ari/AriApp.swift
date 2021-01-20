321f0870ab4734bb8607127b0bcced1a6ff29e2e
kokong <kokong1231@gmail.com>
2021-01-20 15:25:51 +0900

    Initial Commit

---
+++ Ari/AriApp.swift
@@ -0,0 +1,17 @@
+//
+//  AriApp.swift
+//  Ari
+//
+//  Created by 오지훈 on 2021/01/20.
+//
+
+import SwiftUI
+
+@main
+struct AriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ari
+//
+//  Created by 오지훈 on 2021/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
